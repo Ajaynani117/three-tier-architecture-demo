1d8290282cee27bbe82e08e9e11671dbd4475e7d
Ajaynani117 <108183712+Ajaynani117@users.noreply.github.com>
2026-08-14 15:15:03 -0500

    Change default VM size for node pool

--- Azure/terraform/modules/aks/variables.tf
+++ Azure/terraform/modules/aks/variables.tf
@@ -46,7 +46,7 @@ variable "application_gateway_id" {
 variable "node_vm_size" {
   description = "VM size used by the system node pool"
   type        = string
-  default     = "Standard DS2 v2"
+  default     = "standard_d1"
 }
 
 variable "node_count" {
